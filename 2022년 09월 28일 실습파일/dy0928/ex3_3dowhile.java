70ae587b40df19b244461ff2446fd76d719da4e0
김기석 <311kskim@gmail.com>
2023-05-11 13:50:21 +0900

    2022년 09월 28일 자바프로그래밍 실습파일
    2022년 09월 28일 자바프로그래밍 실습파일

---
+++ 2022년 09월 28일 실습파일/dy0928/ex3_3dowhile.java
@@ -0,0 +1,13 @@
+package dy0928;
+
+public class ex3_3dowhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		do {
+			System.out.print(i);
+			i++;
+		 } while(i<10);
+	}
+}
